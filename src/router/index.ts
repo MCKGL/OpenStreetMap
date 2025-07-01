a0f034a127ab81ea171636e78eae6fd8487c1d4e
MCKGL <gries.leguen@gmail.com>
2025-07-01 10:58:53 +0200

    debug mobile et focus

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 const Carto = () => import('@/components/carto/PageCarto.vue');
-const Detail = () => import('@/components/detail/StructureDetail.vue');
 
 const routes = [
   { path: '/', name: 'Carto', component: Carto },
-  { path: '/:slug', component: Detail },
 ];
 
 const router = createRouter({
